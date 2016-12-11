21fca3964f5b57b332a3982643d4cf5f9402462d
regnaper <iv_vod@mail.ru>
2016-12-11 18:27:28 +0300

    The end. May be

--- background.js
+++ background.js
@@ -3,20 +3,15 @@ chrome.contextMenus.create({id: "mymenudecode", title: "Decode this", contexts:[
 
 function genericOnClick(info, tab) {
     var menu = info.menuItemId,
-        decode_flag,
-        key;
+        decode_flag;
     if (menu == "mymenudecode") decode_flag = true;
         else if (menu == "mymenucode") decode_flag = false;
     if (menu == "mymenudecode" || menu == "mymenucode") {
-        var storage = chrome.storage.local;
-        storage.get('key',function(result){
-            key = result['key'];
-        });
         var text = info.selectionText;
         chrome.tabs.executeScript(null,
             {"file": "jquery.js"},
             function() {
-                chrome.tabs.sendMessage(tab.id,{"msg_text":text, "msg_key":key, "decode_flag":decode_flag});
+                chrome.tabs.sendMessage(tab.id,{"msg_text":text, "decode_flag":decode_flag});
             });
     }
 }
--- content_script.js
+++ content_script.js
@@ -1,49 +1,66 @@
-﻿function get_coordinates( event ) {
-    x = event.pageX;
-    y = event.pageY - $(window).scrollTop();
-}
-
-function show_popup(x, y, text){
-    $('#popup').text(text);
-    var popup_h = $("#popup").outerHeight(),
-        popup_w = $("#popup").outerWidth(),
-        display_h = $( window ).height() - 50,
-        display_w = $( window ).width() - 50;
+﻿function show_popup(x, y, text){
+    var popup = $('#popup');
+    popup.text(text);
+    var popup_h = popup.outerHeight(),
+        popup_w = popup.outerWidth(),
+        display_h = $(window).height() - 50,
+        display_w = $(window).width() - 50;
     if (x + popup_w > display_w) x -= popup_w;
     if (y + popup_h > display_h) y -= popup_h;
-    $('#popup').show("slow").offset({top: y + $(window).scrollTop(), left: x});
+    popup.show("slow").offset({top: y + $(window).scrollTop(), left: x});
 }
+
+function get_popup_coordinates(){
+    sel = window.getSelection && window.getSelection();
+    if (sel && sel.rangeCount > 0) {
+        var r = sel.getRangeAt(0).getBoundingClientRect();
+        y = r.bottom;
+        x = r.left;
+    }
+}
+
+var sel = null,
+    x, y;
 
 $('body').append('<div id="popup" style="display: none; position: absolute; overflow: auto "></div>');
 
-$( document ).contextmenu(get_coordinates);
-
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    var text = msg.msg_text;
-    var key = msg.msg_key;
-    if (msg.decode_flag) text = code(text, key, true);
+    get_popup_coordinates();
+    get_key(function(key){
+        var text = msg.msg_text;
+        if (msg.decode_flag) text = code(text, key, true);
         else text = code(text, key, false);
-    show_popup(x, y, text);
-    sendResponse();
+        show_popup(x, y, text);
+        sendResponse();
+    });
 });
 
 $(document).mouseup(function (e)
 {
-    var container = $("#popup");
+    var container = $('#popup');
 
     if (!container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
     {
         container.hide("slow");
+        sel = null;
     }
 });
-$(document).bind('keydown', 'ctrl+q', function(){
-    var storage = chrome.storage.local,
-        key;
-    storage.get('key', function(result){
-        key = result.key;
+
+$(document).bind('keydown', 'shift+d', function () {
+    get_popup_coordinates();
+    var text = sel.toString();
+    if (sel != null) get_key(function (key) {
+        text = code(text, key, true);
+        show_popup(x, y, text);
     });
-    var text = window.getSelection();
-    text = code(text, key, false);
-    console.log(text);
+});
+
+$(document).bind('keydown', 'shift+c', function () {
+    get_popup_coordinates();
+    var text = sel.toString();
+    if (sel != null) get_key(function (key) {
+        text = code(text, key, false);
+        show_popup(x, y, text);
+    });
 });
